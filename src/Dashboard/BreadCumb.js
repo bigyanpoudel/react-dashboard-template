84506077194d3aaad86e026389513f50a71cecfd
bigyanpoudel <46457363+bigyanpoudel@users.noreply.github.com>
2022-07-28 21:06:02 +0545

    added package

--- src/Dashboard/BreadCumb.js
+++ src/Dashboard/BreadCumb.js
@@ -1,19 +1,19 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
-import Link from 'next/link'
-import {useRouter} from 'next/router'
-const BreadCumb = ({rootTitle,currentTitle}) => {
-    const router = useRouter()
+
+const BreadCumb = ({ rootTitle, currentTitle }) => {
   return (
-    <Breadcrumbs aria-label="breadcrumb" style={{marginBottom:"2rem"}}>
-      <Link href="/" style={{color:"#25233c"}} >
+    <Breadcrumbs aria-label="breadcrumb" style={{ marginBottom: "2rem" }}>
+      <a href="/" style={{ color: "#25233c" }}>
         {rootTitle}
-      </Link>
-      <Link color="inherit" href="/getting-started/installation/">
+      </a>
+      <a color="inherit" href="/getting-started/installation/">
         Back
-      </Link>
-      <Typography style={{color:"#6259ca !important"}}>{currentTitle}</Typography>
+      </a>
+      <Typography style={{ color: "#6259ca !important" }}>
+        {currentTitle}
+      </Typography>
     </Breadcrumbs>
   );
 };
